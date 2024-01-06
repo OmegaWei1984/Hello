c639a8fa0754b273d09c5667f4f45e9108534d76
OmegaWei1984 <omegawei1984@gmail.com>
2024-01-06 22:21:37 +0800

    memory/size
    	sizeof(class)

--- cpp/memory/size.cpp
+++ cpp/memory/size.cpp
@@ -21,6 +21,20 @@ struct alignas(16) test3
     int i2;
 };
 
+class Test4
+{
+    int i;
+    int i2;
+    long l;
+};
+
+class Test5
+{
+    int i;
+    long l;
+    int i2;
+};
+
 int main()
 {
     // 16 = 4 + 4 + 8
@@ -31,6 +45,9 @@ int main()
     int size2 = sizeof(test2);
     // 32 = 16 + 16
     int size3 = sizeof(test3);
-
+    // 16 = 4 + 4 + 8
+    size1 = sizeof(Test4);
+    // 24 = 8 + 8 + 8
+    size2 = sizeof(Test5);
     return 0;
 }
