2fb2f33d4dc8c4c6e377b09eff67b9d57b71d265
OmegaWei1984 <omegawei1984@gmail.com>
2024-01-08 15:46:43 +0800

    string strncpy

--- cpp/containers_library/string.cpp
+++ cpp/containers_library/string.cpp
@@ -1,7 +1,24 @@
 #include <string>
 #include <cassert>
+// #include <cstring>
 
 using namespace std;
+
+char *strncpy(char *destination, const char *source, size_t num)
+{
+    char *tmp = destination;
+    while (num > 0)
+    {
+        if ((*tmp = *source) == '\0')
+        {
+            break;
+        }
+        source++;
+        tmp++;
+        num--;
+    }
+    return destination;
+}
 
 int main()
 {
@@ -11,5 +28,8 @@ int main()
     assert(s2.capacity() == 15);
     string s3(6, '0');
     assert(s3.capacity() == 15);
+    char *h = "hello";
+    char h2[6];
+    strncpy(h2, h, 6);
     return 0;
 }
