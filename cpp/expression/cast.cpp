dcf47021f29bbfe2007c4d7fab629f64751594e6
OmegaWei1984 <omegawei1984@gmail.com>
2024-01-08 20:41:23 +0800

    expression/cast
    	static_cast
    	const_cast

--- cpp/expression/cast.cpp
+++ cpp/expression/cast.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <string>
 
@@ -45,13 +46,33 @@ public:
     }
 };
 
+class D
+{
+private:
+    int value{0};
+
+public:
+    void setVal(int val)
+    {
+        value = val;
+    }
+    void test() const
+    {
+        cout << "before: " << value << endl;
+        // value--; // error: decrement of member ‘D::value’ in read-only object
+        const_cast<D*>(this)->value--;
+        cout << "after: " << value << endl;
+    }
+};
+
 int main()
 {
     A *pa = new B();
     A *pa2 = new A();
-    A &ref_a= *pa;
+    A &ref_a = *pa;
     A &ref_a2 = *pa2;
     B *pb = dynamic_cast<B *>(pa);
+    B *pb2 = new B();
 
     // virtual void B::test()
     pa->test();
@@ -80,7 +101,29 @@ int main()
         cout << b.what() << endl;
     }
 
+    C *pc = static_cast<C *>(pb);
+    // virtual void B::test()
+    pc->test();
+    // void C::testtest()
+    pc->testtest();
+    A *pa3 = static_cast<A *>(pb);
+    // virtual void B::test()
+    pa3->test();
+    // void A::testtest()
+    pa3->testtest();
+
+    D d;
+    d.setVal(42);
+    d.test();
+
+    int i = 42;
+    const int &ref_i = i;
+    const_cast<int&>(ref_i) = 1;
+    assert(ref_i == 1);
+    assert(i != 42 && i == 1);
+
     delete pb;
+    delete pb2;
     // delete pc; // free(): double free detected in tcache 2
 
     return 0;
