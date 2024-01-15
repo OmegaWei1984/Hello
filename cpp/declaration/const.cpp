7e45d2aea8ed0ddcacb4822cdbdc4c30d0b58ab4
OmegaWei1984 <omegawei1984@gmail.com>
2024-01-15 14:12:58 +0800

    cpp: const mutable volatile

--- cpp/declaration/const.cpp
+++ cpp/declaration/const.cpp
@@ -8,11 +8,20 @@ class Foo
 {
 private:
     const int MAX = 1;
-    int m_value;
+    int m_value = 42;
 public:
     int get() const
     {
         return m_value;
+    }
+    void set(int value)
+    {
+        m_value = value;
+    }
+    void set2(int value) const
+    {
+        // error: assignment of member ‘Foo::m_value’ in read-only object
+        // m_value = value;
     }
 };
 
@@ -28,6 +37,14 @@ int main()
     assert(num == 43);
     const int *const p3_num = &num;
     // (*p3_num)++; // error: increment of read-only location ‘*(const int*)p3_num’
+
+    Foo foo1;
+    int num2 = foo1.get();
+    const Foo foo2;
+    // error: passing ‘const Foo’ as ‘this’ argument discards qualifiers
+    // foo2.set(10);
+    // error: assignment of member ‘Foo::m_value’ in read-only object
+    // foo2.set2(10);
 
     return 0;
 }
--- cpp/declaration/mutable.cpp
+++ cpp/declaration/mutable.cpp
@@ -1,3 +1,9 @@
+/**
+ * volatile 可以用来修饰任何变量，
+ * 而 mutable 却只能修饰类里面的成员变量，
+ * 表示变量即使是在 const 对象里，也是可以修改的。
+ */
+
 #include "lib/common.hpp"
 
 class Foo
@@ -20,5 +26,9 @@ int main()
     Foo foo;
     foo.set(42);
     assert(foo.get() == 42);
+
+    const Foo kFoo;
+    kFoo.set(10);
+
     return 0;
 }
